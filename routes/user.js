34f5ac694faae484e396774f421c2f7b0497eb05
Anthony Littlejohn <ant05man@yahoo.com>
2024-05-28 13:13:37 -0400

    added code to register new user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 // Register User
@@ -13,9 +15,23 @@ router.post('/register', async (req, res) => {
     }
 
     user = new User({ username, password, email, address });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
     await user.save();
 
-    res.send('User registered');
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(payload, 'secret', { expiresIn: 3600 }, (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    });
+
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
