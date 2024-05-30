8c05cae0b0b4f34b3cc40f7d4868dd9d5ceb424c
Anthony Littlejohn <ant05man@yahoo.com>
2024-05-29 20:46:00 -0400

    added product Detail to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
+      <div>
         <Switch>
-          <Route exact path="/" component={ProductList} />
-          {/* Define other routes */}
+          <Route path="/" exact component={ProductList} />
+          <Route path="/products/:id" component={ProductDetail} />
         </Switch>
       </div>
     </Router>
