0dd847b82a6a1eaaf2744c973b7f1ee18ca3a8eb
Anthony Littlejohn <ant05man@yahoo.com>
2024-05-29 22:39:46 -0400

    edited package.json

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,24 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const baseURL = 'http://localhost:5000/api';
 
-export const fetchProducts = () => API.get('/products');
-export const registerUser = (userData) => API.post('/users/register', userData);
+const axiosInstance = axios.create({
+  baseURL,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    // Handle errors here
+    console.error('API Request Error:', error);
+    throw error; // Rethrow the error to be caught by the caller
+  }
+);
+
+export const productAPI = {
+  fetchProducts: () => axiosInstance.get('/products'),
+};
+
+export const userAPI = {
+  registerUser: (userData) => axiosInstance.post('/users/register', userData),
+};
