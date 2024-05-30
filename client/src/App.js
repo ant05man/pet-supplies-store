bd6a0a1da96d65f44fbf682f8f40af44113bfc99
Anthony Littlejohn <ant05man@yahoo.com>
2024-05-29 20:57:23 -0400

    updated APp.js to include other new components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={ProductList} />
           <Route path="/products/:id" component={ProductDetail} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
         </Switch>
       </div>
     </Router>
