8ac9105853bd4a1b8e12c6314ac075f8c03d510e
Anthony Littlejohn <ant05man@yahoo.com>
2024-05-29 20:51:13 -0400

    created signup page

---
+++ client/src/components/Signup.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const { name, email, password } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/api/users/register', formData);
+      console.log(res.data);
+      // Redirect or show success message
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" value={name} onChange={onChange} placeholder="Name" required />
+      <input type="email" name="email" value={email} onChange={onChange} placeholder="Email" required />
+      <input type="password" name="password" value={password} onChange={onChange} placeholder="Password" required />
+      <button type="submit">Signup</button>
+    </form>
+  );
+};
+
+export default Signup;
